f00fbac7705580ee95d40a5c92f628f08b808b80
kokoriru <yuka@ryukalice.com>
2021-11-11 13:43:15 +0900

    Row を用いて横並びの アイコン+名称 という形でヘッダータイトルを 設定

--- lib/main.dart
+++ lib/main.dart
@@ -73,8 +73,14 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+        title: Row(children: [
+          Icon(
+            Icons.create,
+            color: Colors.blue[100]
+          ),
+          Text('初めてのタイトル'),
+        ],
+      )),
       body: Text('初めてのテキスト'),
     );
   }
